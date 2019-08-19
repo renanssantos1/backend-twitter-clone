19fdf5b2dfbd2e2ce2dee09345bdf21d1eade88a
Renan da Silva <renan.serverti@gmail.com>
2019-08-19 19:55:43 -0300

    ADD to socket io and controllers

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 mongoose.connect(
   'mongodb://goweek:goweek123@ds211268.mlab.com:11268/goweek-backend' , {
   useNewUrlParser: true,
-})
+});
 
+app.use((req, res, next) => {
+  req.io = io;
+
+  return next();
+});
+
+app.use(cors());
 app.use(express.json());
 app.use(require('./routes'));
 
-app.listen(3000, () => console.log('Server started on port 3000'));
+server.listen(3000, () => console.log('Server started on port 3000'));
